0a3b67405ee78874106c9c025e8ff622fab16a80
kcmair <97917570+kcmair@users.noreply.github.com>
2024-10-21 21:27:41 -0600

    Merge pull request #3
    * completed exercises
    
    * completed team exercises 3

--- src/js/product.js
+++ src/js/product.js
@@ -1,17 +1,5 @@
-import { findProductById } from "./productData.mjs";
+import { getParam } from './utils.mjs';
+import productDetails from './productDetails.mjs';
 
-function addProductToCart(product) {
-  let existingItems = JSON.parse(localStorage.getItem("so-cart")) || [];
-  existingItems.push(product);
-  localStorage.setItem("so-cart", JSON.stringify(existingItems));
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam();
+productDetails(productId);
